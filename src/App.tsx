e326678fc735fda6be0758949e48ca03b964c396
Marcelo Meirelles <meirellesm@hotmail.com>
2023-11-30 20:45:40 -0300

    feat:add mobile menu

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import { Header } from "./components/Header"
 import { Projects } from "./components/Projects"
 import { Techs } from "./components/Techs"
 import "./globalStyles.scss"
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 
 function App() {
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,29 @@
 import { Button } from "../Button"
 import "./styles.scss"
 import { SiGithub, SiLinkedin, SiWhatsapp } from "react-icons/si"
+import { TiThMenu } from "react-icons/ti";
 import { ImProfile } from "react-icons/im"
+import { useState } from "react"
+import { VscChromeClose } from "react-icons/vsc";
+
+
+
+
 
 export function Header(){
+    const [isOpen, setIsOpen] = useState(false)
+
 
     return (
+        <>
         <header>
             <div className="container div-header">
                 <div className="div-infos">
                     <p className="img">img</p>
                     <p className="name">Marcelo Meirelles</p>
+                </div>
+                <div onClick={()=>setIsOpen(!isOpen)} className="trigger">
+                    <Button classStyle="button" text={isOpen?<VscChromeClose/>:<TiThMenu/>} />
                 </div>
                 <div className="div-buttons">
                     <Button classStyle="button" text={<SiWhatsapp size="20" />} />
@@ -21,5 +34,18 @@ export function Header(){
             </div>
 
         </header>
+        <div className={`mobile-menu ${isOpen && "is-open"}`}>
+                <div> 
+                    <Button classStyle="button mobile-button" text={<SiWhatsapp size="20" />} />
+                </div>
+                <div> 
+                    <Button classStyle="button mobile-button" text={<ImProfile size="20" />} />
+                </div>
+                <div> 
+                    <Button classStyle="button mobile-button second-color border-white" text={<SiGithub size="20" />} />
+                </div>
+        </div>
+        
+        </>
     )
 }
